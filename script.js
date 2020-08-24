d04955a7e76330103b0ec409421468a39cd1e84e
celikfth <60170216+celikfth@users.noreply.github.com>
2020-08-24 14:45:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function myFunction(){
+    window.location = "about.html"
+}
+function turnHome(){
+    window.location = "index.html"
+}
+function contact(){
+    window.location = "contact.html"
+}
+function show(yol)
+{
+var picture=document.querySelector('img');
+picture.src=yol;
+
+}
